e14d87300d43d7e7523f557897c3b056a3888ce6
Shinji-ux <shinji.iwasaki1218@gmail.com>
2024-10-03 17:34:38 +0900

    名前のバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   validates :nickname, presence: true
-  validates :last_name_kanji, presence: true
-  validates :first_name_kanji, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :birth_day, presence: true
+  validates :last_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX
 end
