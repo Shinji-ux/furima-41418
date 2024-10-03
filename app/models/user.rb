0b6346c1fa3932bb4e0d63e8ba94a210da8920eb
Shinji-ux <shinji.iwasaki1218@gmail.com>
2024-10-03 17:40:41 +0900

    Rubocopの実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,6 @@ class User < ApplicationRecord
   validates :first_name_kanji, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
   validates_format_of :password, with: PASSWORD_REGEX
 end
