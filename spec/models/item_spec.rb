03935e0631220c6c404276e973730c60078a9ccb
Shinji-ux <shinji.iwasaki1218@gmail.com>
2024-10-04 22:02:02 +0900

    テストコードの項目追加
    新しいテスト結果の画像です
    
    https://gyazo.com/5f704041951645a1778c52847194cc74

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -62,6 +62,22 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
+      it 'priceが全角を含むと出品できない' do
+        @item.price = '100００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
+      end
+      it '画像が空では出品できない' do
+        @item.image = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Image can't be blank")
+      end
+      it 'userが紐づいていないと出品できない' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
+
     end
   end
 end
