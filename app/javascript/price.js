46af2bd4b0200361815b12ff710f7ee0eb6f248c
Shinji-ux <shinji.iwasaki1218@gmail.com>
2024-10-04 19:46:26 +0900

    JavaScriptの実装

---
+++ app/javascript/price.js
@@ -0,0 +1,14 @@
+const priceInput = document.getElementById("item-price");
+priceInput.addEventListener("input", () => {
+  const inputValue = priceInput.value;
+  
+  const addTaxPrice = Math.floor(inputValue * 0.1);
+  const addTaxPriceArea = document.getElementById("add-tax-price");
+  document.getElementById("add-tax-price").innerHTML = "";
+  addTaxPriceArea.insertAdjacentHTML("afterbegin", addTaxPrice);
+
+  const profit = Math.floor(inputValue * 0.9);
+  const profitArea = document.getElementById("profit");
+  document.getElementById("profit").innerHTML = "";
+  profitArea.insertAdjacentHTML("afterbegin", profit);
+});
