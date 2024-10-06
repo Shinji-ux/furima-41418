bedc515e2a0042dd3c5220989c3776048ddaffdc
Shinji-ux <shinji.iwasaki1218@gmail.com>
2024-10-06 14:53:13 +0900

    コントローラーのアクション内容をprivateへ
    該当部分がちゃんと動いていることを確認した動画です
    
    https://gyazo.com/c2f15c430539241b7e3ed6efa00d28ca

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :move_to_login, only: [:new, :edit]
   before_action :correct_user, only: :edit
+  before_action :set_item, only: [:show, :edit, :update]
 
   def index
     @items = Item.includes(:user).order('created_at DESC')
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -53,5 +51,9 @@ class ItemsController < ApplicationController
     unless @item.user == current_user
       redirect_to root_path
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
